4d19058888174fbcd650b90dd4819801bcfa3ff4
OTTO DIAS <otto.dias@alunos.sc.senac.br>
2026-04-07 14:27:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tabalho_Caxeta',
+  webDir: 'dist'
+};
+
+export default config;
